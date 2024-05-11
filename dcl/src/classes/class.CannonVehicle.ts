f582e6576889ea08d507fae73599fc8bf3ca2f25
stom <tomsteventon@gmail.com>
2024-05-11 21:03:16 +0100

    feat: add child entity to handle rotation tweening, update tween timing

--- dcl/src/classes/class.CannonVehicle.ts
+++ dcl/src/classes/class.CannonVehicle.ts
@@ -27,11 +27,15 @@ export class CannonVehicle {
 	Current speed is lerped up to max speed while the user holds FORWARD, and down to zero otherwise.	
 	*/	
 	
-	entity        : Entity
-	cannonBody    : CANNON.Body
+	entityPos         : Entity 			// Root Entity used to set object position
+	entityRot         : Entity 			// Child Entity used to set object rotation
+	cannonBody        : CANNON.Body 	// Cannon physics body
 	
-	currentSpeed  : number = 0  // This gets lerped between 0 and maxSpeed depending on if W is pressed
-	isAccelerating: boolean = false // Toggled by user pressing/releasing W. Referenced by CannonVehicleInputSystem
+	currentSpeed      : number  = 0  	// This gets lerped between 0 and maxSpeed depending on if W is pressed
+	isAccelerating    : boolean = false // Toggled by user pressing/releasing W. Referenced by CannonVehicleInputSystem
+	
+	tweenDuration     : number  = 50 	// In ms, eg 1 second = 1000
+	timeSinceLastTween: number = 0
 	
 	constructor(
 		world              : CANNON.World,
@@ -41,10 +45,19 @@ export class CannonVehicle {
 		public acceleration: number = 12
 	) {
 		
-		// Set up gltf shape
-		this.entity = engine.addEntity()
-		Transform.create(this.entity, transform)
-		GltfContainer.create(this.entity, {src: modelSrc})
+		// Set up root entity, used for adjusting position
+		this.entityPos = engine.addEntity()
+		Transform.create(this.entityPos, transform)
+		
+		// Setup child entity, used for rotating the vehicle
+		this.entityRot = engine.addEntity()
+		Transform.create(this.entityRot, {
+			position: Vector3.create(0, 0, 0),
+			parent  : this.entityPos,
+		})
+		
+		// Add the gltf shape to the child
+		GltfContainer.create(this.entityRot, {src: modelSrc})
 		
 		// Set up the cannon body to represent the vehicle
 		this.cannonBody = new CANNON.Body({ 
@@ -92,22 +105,13 @@ export class CannonVehicle {
 	}
 }
 
-// Simple function to return a forward vector based on the given yaw value in degrees
-// Takes the current camera yaw value
-function getForwardDirection(yRot: number): CANNON.Vec3 {
-	// Convert to rads
-	yRot = yRot * (Math.PI / 180)
-	
-	// Workout forwards
-    const forward = new CANNON.Vec3(Math.sin(yRot), 0, Math.cos(yRot));
-    forward.normalize();
-	return forward
-}
 
 // Player input system to respond to forward input, and to move vehicle accordingly
 export function CannonVehicleInputSystem(dt: number): void {
-	
+		
 	// Loop through each of the vehicles in the map and handle their inputs and movements
+	// This gives us cannonBody: a reference to the CANNON.Body used by the Class instance,
+	// As well as cannonVehicle, a reference to the Class instance itself (which contains .entity, .entityChild, and .cannonBody)
 	cannonVehicleEntityMap.forEach((cannonBody, cannonVehicle) => {
 		
 		// Handle acceleration/deceleration inputs
@@ -123,6 +127,7 @@ export function CannonVehicleInputSystem(dt: number): void {
 		cannonVehicle.updateSpeed(dt)
 		
 		// Lerp the cannonVehicle.velocity toward the (camera direction * current speed)
+		// We lerp it rather than set it to avoid being able to change direction instantly
 		const cameraEulerRot  = Quaternion.toEulerAngles(getCameraRotation())
 		const targetDirection = getForwardDirection(cameraEulerRot.y)
 		const targetVelocity  = targetDirection.scale(cannonVehicle.currentSpeed)
@@ -130,35 +135,63 @@ export function CannonVehicleInputSystem(dt: number): void {
 		cannonBody.velocity.lerp(targetVelocity, 0.1, cannonBody.velocity)		
 
 		// Calculate the vehicle yaw (rotation around the y-axis) based on the velocity vector
+		// This is used to determine which way the vehicle should face
+		// Rather than just take the camera angle above, which would result in the vehicle "drifting" sideways
 		const yaw      = Math.atan2(targetDirection.x, targetDirection.z);
 		const rotation = new CANNON.Quaternion();
 		rotation.setFromAxisAngle(new CANNON.Vec3(0, 1, 0), yaw);
 		
 		// Apply the position and rotation to the vehicle
-		const transform = Transform.getMutable(cannonVehicle.entity);		
-		if (transform) {
-
- 			Tween.createOrReplace(cannonVehicle.entity, {
-				mode: Tween.Mode.Move({
-					start: transform.position,
-					end  : cannonBody.position.clone()
-				}),
-				duration: 10,
-				easingFunction: EasingFunction.EF_LINEAR,
-			})
 			
-			/* Tween.createOrReplace(cannonVehicle.entity, {
-				mode: Tween.Mode.Rotate({
-					start: transform.rotation,
-					end  : rotation
-				}),
-				duration: 10,
-				easingFunction: EasingFunction.EF_LINEAR,
-			})
- 			*/
+			cannonVehicle.timeSinceLastTween += (dt * 1000)
+			if (cannonVehicle.timeSinceLastTween > cannonVehicle.tweenDuration) {
+				cannonVehicle.timeSinceLastTween = 0
 			
-			//transform.position = cannonBody.position
-			transform.rotation = rotation 
+				// Ensure we have access to the transform component before trying to set the Tween, or we get an error when the vehicle first loads
+				const transformPos = Transform.getMutable(cannonVehicle.entityPos);
+				
+				if (transformPos) {
+					// Start Tween on the vehicle parent entity
+					Tween.createOrReplace(cannonVehicle.entityPos, {
+						mode: Tween.Mode.Move({
+							start: transformPos.position,
+							end  : cannonBody.position.clone()
+						}),
+						duration: cannonVehicle.tweenDuration,
+						easingFunction: EasingFunction.EF_LINEAR,
+					})
+				}
+				
+				const transformRot = Transform.getMutable(cannonVehicle.entityRot);
+				if (transformRot) {
+					
+					// Start a separate tween on the Child to rotate it
+					Tween.createOrReplace(cannonVehicle.entityRot, {
+						mode: Tween.Mode.Rotate({
+							start: transformRot.rotation,
+							end  : rotation
+						}),
+						duration: cannonVehicle.tweenDuration,
+						easingFunction: EasingFunction.EF_LINEAR,
+					})	
+					
+					//transform.position = cannonBody.position
+					//transform.rotation = rotation 
+				}			
+			
+			
 		}
 	});
+}
+
+// Simple function to return a forward vector based on the given yaw value in degrees
+// Takes the current camera yaw value
+function getForwardDirection(yRot: number): CANNON.Vec3 {
+	// Convert to rads
+	yRot = yRot * (Math.PI / 180)
+	
+	// Workout forwards
+    const forward = new CANNON.Vec3(Math.sin(yRot), 0, Math.cos(yRot));
+    forward.normalize();
+	return forward
 }
