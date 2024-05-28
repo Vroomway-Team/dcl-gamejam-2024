cde8e030a4f6dd0da0cb8e3cf74741e4120dd27a
Vroomway <137978324+Nikki-Fuego@users.noreply.github.com>
2024-05-27 23:16:43 -0500

    update: arena development + dash graphic

--- dcl/src/arena/setupGltfShapes.ts
+++ dcl/src/arena/setupGltfShapes.ts
@@ -5,7 +5,19 @@ import { GltfObject } 			from "../classes/class.GltfObject";
 export function setupGltfShapes() {
 	
 	// Spawn the arena
-	const arena = new GltfObject("assets/gltf/arena.base.gltf", {
+	const arena0 = new GltfObject("assets/gltf/arena.base.gltf", {
+		position: Vector3.create(0, 0, 0),
+		rotation: Quaternion.fromEulerDegrees(0, 180, 0),
+		scale   : Vector3.One()
+	}, ColliderLayer.CL_PHYSICS, ColliderLayer.CL_PHYSICS)
+
+	const arena1 = new GltfObject("assets/gltf/arena.top.gltf", {
+		position: Vector3.create(0, 0, 0),
+		rotation: Quaternion.fromEulerDegrees(0, 180, 0),
+		scale   : Vector3.One()
+	}, ColliderLayer.CL_PHYSICS, ColliderLayer.CL_PHYSICS)
+
+	const arena2 = new GltfObject("assets/gltf/arena.roof.gltf", {
 		position: Vector3.create(0, 0, 0),
 		rotation: Quaternion.fromEulerDegrees(0, 180, 0),
 		scale   : Vector3.One()
