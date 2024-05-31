4ab0e5ccafd2e6b11b9bc6966826456190f39a1a
stom <tomsteventon@gmail.com>
2024-05-31 16:10:38 +0100

    fix: rpevent spamming new entities when updating scoreboard, prevent multiple sortScores from triggering

--- dcl/src/interfaces/interface.Scoreboard.ts
+++ dcl/src/interfaces/interface.Scoreboard.ts
@@ -1,7 +1,8 @@
 export interface ScoreboardState {
 	roundInProgress?: boolean,           // Current round state, "Match in progress", "Match about to start", etc
 	roundTimer?     : number, 			// elapsed round time in seconds
-	scores         : ScoreboardEntry[], // array of ScoreboardEntry's
+	scores          : ScoreboardEntry[], // array of ScoreboardEntry's
+	storesSorted    : boolean
 }
 
 export interface ScoreboardEntry {
